60200d97ef9e751d13f7568485eef104c646540d
Taufik Nurrohman <hi@taufik-nurrohman.com>
2022-10-14 11:23:15 +0700

    Update

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
 
 $content = file_get_contents(__DIR__ . D . 'test');
 
-echo '<pre style="border:3px solid #900;padding:3px;white-space:pre-wrap;word-wrap:break-word;"><code>' . htmlspecialchars($content) . '</code></pre>';
-echo '<pre style="border:3px solid #090;padding:3px;white-space:pre-wrap;word-wrap:break-word;"><code>' . htmlspecialchars(fire("x\\p", [$content], (object) ['type' => 'HTML'])) . '</code></pre>';
+echo '<pre style="background:#ccc;border:1px solid rgba(0,0,0,.25);color:#000;font:normal normal 100%/1.25 monospace;padding:.5em .75em;white-space:pre-wrap;word-wrap:break-word;">' . htmlspecialchars($content) . '</pre>';
+echo '<pre style="background:#cfc;border:1px solid rgba(0,0,0,.25);color:#000;font:normal normal 100%/1.25 monospace;padding:.5em .75em;white-space:pre-wrap;word-wrap:break-word;">' . htmlspecialchars(fire("x\\p", [$content], (object) ['type' => 'HTML'])) . '</pre>';
 
 exit;
